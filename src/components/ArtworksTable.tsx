086cb995d5f87c6dfdb78302f74ce9090dc6991c
ashishmaurya57 <ashishmauryaam57@gmail.com>
2024-09-16 20:24:13 +0530

    Preparing for GitHub Pages deployment

--- src/components/ArtworksTable.tsx
+++ src/components/ArtworksTable.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { DataTable, DataTablePageEvent } from 'primereact/datatable';
+import { DataTable, DataTableSelectionChangeEvent, DataTablePageEvent } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { InputSwitch } from 'primereact/inputswitch';
 import { OverlayPanel } from 'primereact/overlaypanel';
